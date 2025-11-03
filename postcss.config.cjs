5c8108742fe280979817b12aa8d4fb095db50e30
Aji-ene <77490590+Aji-ene@users.noreply.github.com>
2025-11-03 08:36:25 -0800

    Update change postcss.config.cjs

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,7 +1,7 @@
 // filepath: c:\Users\DELL\untamed-collections\postcss.config.js
 module.exports = {
   plugins: {
-    '@tailwindcss/postcss': {},
+    tailwindcss: {},
     autoprefixer: {},
   },
 }
